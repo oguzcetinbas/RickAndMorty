abfde24f588b1098735b1ede4d39e83dd563fb2f
oguzcetinbas <oguzctnbs94@gmail.com>
2023-06-03 16:30:35 +0300

    added splash screen

---
+++ app/src/main/java/com/example/rickandmorty/ui/list/SplashActivity.kt
@@ -0,0 +1,53 @@
+package com.example.rickandmorty.ui.list
+
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.content.Intent
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import com.example.rickandmorty.R
+import com.example.rickandmorty.ui.MainActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_DELAY: Long = 5000
+    private val splashScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val scaleAnim = findViewById<ImageView>(R.id.imageRickyView)
+
+        val scaleAnimation =
+            AnimatorInflater.loadAnimator(this, R.anim.scale_animation) as AnimatorSet
+
+        scaleAnimation.setTarget(scaleAnim)
+        scaleAnimation.start()
+
+        splashScope.launch {
+            delay(SPLASH_DELAY)
+            navigateToMainActivity()
+        }
+    }
+
+    private fun navigateToMainActivity() {
+        val mainIntent = Intent(this@SplashActivity, MainActivity::class.java)
+        startActivity(mainIntent)
+        finish()
+    }
+
+    override fun onDestroy() {
+        splashScope.cancel()
+        super.onDestroy()
+    }
+}
